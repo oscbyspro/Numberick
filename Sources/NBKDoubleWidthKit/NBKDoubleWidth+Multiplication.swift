81452a7182a02bfa851f74f6c978be124ecd5635
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-06-06 19:51:00 +0200

    Cleanup.

--- Sources/NBKDoubleWidthKit/NBKDoubleWidth+Multiplication.swift
+++ Sources/NBKDoubleWidthKit/NBKDoubleWidth+Multiplication.swift
@@ -113,31 +113,3 @@ extension NBKDoubleWidth where High == High.Magnitude {
         return HL<Self, Magnitude>(high: hi, low: lo)
     }
 }
-
-
-//*============================================================================*
-// MARK: * ANK x Arithmetic x Unsigned x Fixed Width
-//*============================================================================*
-
-extension NBKFixedWidthInteger where Self: NBKUnsignedInteger {
-    
-    //=------------------------------------------------------------------------=
-    // MARK: Transformations
-    //=------------------------------------------------------------------------=
-    
-    /// Returns the sum of given values.
-    ///
-    /// ```swift
-    /// UInt.sum(~0,  1,  2) // (high: 1, low:  2)
-    /// UInt.sum(~0, ~0, ~0) // (high: 2, low: ~2)
-    /// ```
-    ///
-    /// - Returns: A value in the range: `(high: 0, low: 0) ... (high: 2, low: ~2)`.
-    ///
-    @inlinable static func sum(_ x0: Self, _ x1: Self, _ x2: Self) -> HL<UInt, Self> {
-        var xx = x0
-        let o3 = xx.addReportingOverflow(x1)
-        let o4 = xx.addReportingOverflow(x2)
-        return HL(UInt(bit: o3) &+ UInt(bit: o4), xx)
-    }
-}
