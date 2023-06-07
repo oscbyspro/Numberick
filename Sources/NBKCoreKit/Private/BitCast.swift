b4c61430400a5d59e4563376d76cccfc22c1b1fa
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-06-07 09:48:11 +0200

    Bump.

--- Sources/NBKCoreKit/Private/BitCast.swift
+++ Sources/NBKCoreKit/Private/BitCast.swift
@@ -17,23 +17,27 @@ extension NBK {
     // MARK: Utilities
     //=------------------------------------------------------------------------=
     
+    /// Reinterprets the bits of a partial value and an overflow indicator.
     @inlinable public static func bitCast<A0, B0>(_ x: PVO<A0>) -> PVO<B0> where
     A0: NBKBitPatternConvertible, B0: NBKBitPatternConvertible, A0.BitPattern == B0.BitPattern {
-        PVO(partialValue: B0(bitPattern: x.partialValue), x.overflow)
+        PVO(partialValue: B0(bitPattern: x.partialValue), overflow: x.overflow)
     }
     
+    /// Reinterprets the bits of a high and a low value.
     @inlinable public static func bitCast<A0, A1, B0, B1>(_ x: HL<A0, A1>) -> HL<B0, B1> where
     A0: NBKBitPatternConvertible, B0: NBKBitPatternConvertible, A0.BitPattern == B0.BitPattern,
     A1: NBKBitPatternConvertible, B1: NBKBitPatternConvertible, A1.BitPattern == B1.BitPattern {
-        HL(B0(bitPattern: x.high), B1(bitPattern: x.low))
+        HL(high: B0(bitPattern: x.high), low: B1(bitPattern: x.low))
     }
     
+    /// Reinterprets the bits of a quotient and a remainder, relating to division.
     @inlinable public static func bitCast<A0, A1, B0, B1>(_ x: QR<A0, A1>) -> QR<B0, B1> where
     A0: NBKBitPatternConvertible, B0: NBKBitPatternConvertible, A0.BitPattern == B0.BitPattern,
     A1: NBKBitPatternConvertible, B1: NBKBitPatternConvertible, A1.BitPattern == B1.BitPattern {
         QR(quotient: B0(bitPattern: x.quotient), remainder: B1(bitPattern: x.remainder))
     }
     
+    /// Reinterprets the bits of a partial value and an overflow indicator.
     @inlinable public static func bitCast<A0, A1, B0, B1>(_ x: PVO<QR<A0, A1>>) -> PVO<QR<B0, B1>> where
     A0: NBKBitPatternConvertible, B0: NBKBitPatternConvertible, A0.BitPattern == B0.BitPattern,
     A1: NBKBitPatternConvertible, B1: NBKBitPatternConvertible, A1.BitPattern == B1.BitPattern {
