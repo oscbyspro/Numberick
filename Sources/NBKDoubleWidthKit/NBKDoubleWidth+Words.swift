1582a92acc74b0575486c7eeb9a9375eee28d591
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-06-16 16:29:45 +0200

    No count-is-power-of-two guarantee (#14)

--- Sources/NBKDoubleWidthKit/NBKDoubleWidth+Words.swift
+++ Sources/NBKDoubleWidthKit/NBKDoubleWidth+Words.swift
@@ -122,10 +122,9 @@ extension NBKDoubleWidth {
     /// Accesses the word at the given index, from least significant to most.
     ///
     /// ``NBKDoubleWidth`` models a trivial `UInt` collection, where `UInt` is an
-    /// unsigned machine word. It contains at least two words, and its word count
-    /// is always a power of two. This layout enables direct machine word access.
-    /// The exact count depends on the platform's architecture. You should, therefore,
-    /// use properties like `startIndex` and `endIndex` instead of hard-coded indices.
+    /// unsigned machine word. It contains at least two words, but the exact count
+    /// depends on the platform's architecture. You should, therefore, use
+    /// properties like `startIndex` and `endIndex` instead of hard-coded indices.
     ///
     /// ```
     /// // Int256 and UInt256, as constructed on a 64-bit platform:
@@ -146,10 +145,9 @@ extension NBKDoubleWidth {
     /// Accesses the word at the given index, from least significant to most.
     ///
     /// ``NBKDoubleWidth`` models a trivial `UInt` collection, where `UInt` is an
-    /// unsigned machine word. It contains at least two words, and its word count
-    /// is always a power of two. This layout enables direct machine word access.
-    /// The exact count depends on the platform's architecture. You should, therefore,
-    /// use properties like `startIndex` and `endIndex` instead of hard-coded indices.
+    /// unsigned machine word. It contains at least two words, but the exact count
+    /// depends on the platform's architecture. You should, therefore, use
+    /// properties like `startIndex` and `endIndex` instead of hard-coded indices.
     ///
     /// ```
     /// // Int256 and UInt256, as constructed on a 64-bit platform:
--- Sources/NBKDoubleWidthKit/NBKDoubleWidth.swift
+++ Sources/NBKDoubleWidthKit/NBKDoubleWidth.swift
@@ -46,10 +46,9 @@ import NBKCoreKit
 /// ### 📖 Trivial UInt Collection
 ///
 /// ``NBKDoubleWidth`` models a trivial `UInt` collection, where `UInt` is an
-/// unsigned machine word. It contains at least two words, and its word count
-/// is always a power of two. This layout enables direct machine word access.
-/// The exact count depends on the platform's architecture. You should, therefore,
-/// use properties like `startIndex` and `endIndex` instead of hard-coded indices.
+/// unsigned machine word. It contains at least two words, but the exact count
+/// depends on the platform's architecture. You should, therefore, use
+/// properties like `startIndex` and `endIndex` instead of hard-coded indices.
 ///
 /// ```
 /// // Int256 and UInt256, as constructed on a 64-bit platform:
