4967b6b84f77776d5090f1db5f6f45762e3777b7
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-06-18 13:55:31 +0200

    10% faster imperfect radix decoding (#17)

--- Sources/NBKDoubleWidthKit/NBKDoubleWidth+Text+Radix.swift
+++ Sources/NBKDoubleWidthKit/NBKDoubleWidth+Text+Radix.swift
@@ -75,8 +75,8 @@ extension NBKDoubleWidth where High == High.Magnitude {
                     value[index] = UInt.zero
                 }   else {
                     let chunk = NBK.removeSuffix(from: &digits, maxLength: radix.exponent)
-                    let word  = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base)
-                    if let word { value[index] = word } else { return error = true }
+                    guard let word = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base) else { return error = true }
+                    value[index] = word
                 }
             }
         }
@@ -90,20 +90,27 @@ extension NBKDoubleWidth where High == High.Magnitude {
         var digits = digits.drop(while:{ $0 == 48 })
         let alignment = digits.count % radix.exponent
         //=--------------------------------------=
-        self.init()
-        
-        forwards: if !alignment.isZero {
-            let chunk = NBK.removePrefix(from: &digits, maxLength: alignment)
-            guard let word = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base) else { return nil }
-            self.first = word // self = self * radix.power + word
+        var error = false
+        let value = Self.uninitialized { value in
+            for index in value.indices {
+                value[index] = UInt.zero
+            }
+            
+            forwards: if !alignment.isZero {
+                let chunk = NBK.removePrefix(from: &digits, maxLength: alignment)
+                guard let word = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base) else { return error = true }
+                value.first = word
+            }
+            
+            forwards: while !digits.isEmpty {
+                let chunk = NBK.removePrefix(from: &digits, maxLength: radix.exponent)
+                guard let word = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base) else { return error = true }
+                guard !value.multiplyReportingOverflow(by: radix.power) else { return error = true }
+                guard !value.addReportingOverflow(word)/*------------*/ else { return error = true }
+            }
         }
         
-        forwards: while !digits.isEmpty {
-            let chunk = NBK.removePrefix(from: &digits, maxLength: radix.exponent)
-            guard let word = UInt(digits: NBK.UnsafeUTF8(rebasing: chunk), radix: radix.base) else { return nil }
-            guard !self.multiplyReportingOverflow(by: radix.power) else { return nil }
-            guard !self.addReportingOverflow(word)/*------------*/ else { return nil }
-        }
+        if !error { self = value } else { return nil }
     }
     
     //=------------------------------------------------------------------------=
