cbb8f3f4425b39927501dae6db351036cfedd947
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-07-31 13:50:52 +0200

    Cleanup.

--- Sources/NBKCoreKit/Private/Limbs.swift
+++ Sources/NBKCoreKit/Private/Limbs.swift
@@ -17,11 +17,11 @@ extension NBK {
     // MARK: Utilities
     //=------------------------------------------------------------------------=
     
-    @inlinable public static func limbs<A, B>(_ other: A, isSigned: Bool = false, as type: B.Type = B.self) -> B where
+    @inlinable public static func limbs<A, B>(_ source: A, isSigned: Bool = false, as type: B.Type = B.self) -> B where
     A:  RandomAccessCollection, A.Element: NBKCoreInteger, B: RangeReplaceableCollection & MutableCollection, B.Element: NBKCoreInteger {
         switch A.Element.bitWidth >= B.Element.bitWidth {
-        case  true: return NBK.minorLimbs(majorLimbs: other, isSigned: isSigned)
-        case false: return NBK.majorLimbs(minorLimbs: other, isSigned: isSigned) }
+        case  true: return NBK.minorLimbs(majorLimbs: source, isSigned: isSigned)
+        case false: return NBK.majorLimbs(minorLimbs: source, isSigned: isSigned) }
     }
     
     @inlinable static func minorLimbs<A, B>(majorLimbs: A, isSigned: Bool = false, as type: B.Type = B.self) -> B where
@@ -29,17 +29,17 @@ extension NBK {
         precondition(A.Element.bitWidth.isPowerOf2)
         precondition(A.Element.bitWidth >= B.Element.bitWidth)
         //=--------------------------------------=
-        let ratio: Int  = A.Element.bitWidth / B.Element.bitWidth
-        var destination = B(); destination.reserveCapacity(majorLimbs.count * ratio)
+        let ratio: Int = A.Element.bitWidth / B.Element.bitWidth
+        var minorLimbs = B(); minorLimbs.reserveCapacity(majorLimbs.count * ratio)
         //=--------------------------------------=
         for var majorLimb in majorLimbs {
             for _ in 0 ..< ratio {
-                destination.append(B.Element(truncatingIfNeeded: majorLimb))
+                minorLimbs.append(B.Element(truncatingIfNeeded: majorLimb))
                 majorLimb &>>= B.Element.bitWidth
             }
         }
         //=--------------------------------------=
-        return destination as B
+        return (minorLimbs) as B
     }
     
     @inlinable static func majorLimbs<A, B>(minorLimbs: A, isSigned: Bool = false, as type: B.Type = B.self) -> B where
@@ -49,27 +49,25 @@ extension NBK {
         //=--------------------------------------=
         let ratio: Int = B.Element.bitWidth / A.Element.bitWidth
         let count = minorLimbs.count.quotientAndRemainder(dividingBy: ratio)
-        var destination = B(repeating: B.Element.zero, count: count.quotient + Int(bit: !count.remainder.isZero))
+        var majorLimbs = B(repeating: B.Element.zero, count: count.quotient + Int(bit: !count.remainder.isZero))
         //=--------------------------------------=
-        var major = Int.zero
-        var minor = Int.zero
-                
+        var minorShift = Int.zero
+        var majorIndex = majorLimbs.startIndex as B.Index
+        
         for minorLimb in minorLimbs {
-            let destinationIndex = destination.index(destination.startIndex, offsetBy: major)
-            destination[destinationIndex] |= B.Element(truncatingIfNeeded: minorLimb.bitPattern) &<< minor
-            minor += A.Element.bitWidth
+            majorLimbs[majorIndex] |= B.Element(truncatingIfNeeded: minorLimb.bitPattern) &<< minorShift
+            minorShift += A.Element.bitWidth
             
-            if  minor >= B.Element.bitWidth {
-                minor -= B.Element.bitWidth
-                major += 1
+            if  minorShift >= B.Element.bitWidth {
+                minorShift -= B.Element.bitWidth
+                majorLimbs.formIndex(after: &majorIndex)
             }
         }
         
-        if !minor.isZero, isSigned {
-            let destinationIndex = destination.index(destination.startIndex, offsetBy: major)
-            destination[destinationIndex] |= B.Element(repeating: minorLimbs.last!.mostSignificantBit) &<< minor
+        if !minorShift.isZero, isSigned {
+            majorLimbs[majorIndex] |= B.Element(repeating: minorLimbs.last!.mostSignificantBit) &<< minorShift
         }
         //=--------------------------------------=
-        return destination as B
+        return (majorLimbs) as B
     }
 }
