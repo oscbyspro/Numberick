463c2a026b5f970a86726d6dcdc94af6ef321fe4
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-08-03 13:27:40 +0200

    NBKCoreKit: Bits.

--- Sources/NBKCoreKit/Private/Bits.swift
+++ Sources/NBKCoreKit/Private/Bits.swift
@@ -17,10 +17,12 @@ extension NBK {
     // MARK: Utilities
     //=------------------------------------------------------------------------=
     
+    /// Returns the nonzero bit count of `limbs`.
     @inlinable public static func nonzeroBitCount(of limbs: some Collection<some NBKCoreInteger>) -> Int {
         limbs.reduce(Int.zero) { $0 + $1.nonzeroBitCount }
     }
     
+    /// Returns whether the nonzero bit count of `limbs` equals `comparand`.
     @inlinable public static func nonzeroBitCount(of limbs: some Collection<some NBKCoreInteger>, equals comparand: Int) -> Bool {
         var count = Int()
         var index = limbs.startIndex
@@ -30,6 +32,12 @@ extension NBK {
             limbs.formIndex(after: &index)
         }
         
-        return count == comparand as Int
+        return (count == comparand) as Bool
+    }
+    
+    /// Returns the nonzero bit count for the two's complement of `limbs`.
+    @inlinable public static func nonzeroBitCount(twosComplementOf limbs: some Collection<some NBKCoreInteger>) -> Int {
+        guard let index = limbs.firstIndex(where:{ !$0.isZero }) else { return Int.zero }
+        return limbs.indices[index...].reduce(1 - limbs[index].trailingZeroBitCount) { $0 + limbs[$1].onesComplement().nonzeroBitCount }
     }
 }
