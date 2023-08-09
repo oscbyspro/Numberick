9be56162b687f8ae7304061f4bb45629fd0f4e2f
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-08-09 17:30:31 +0200

    NBKCoreKit: Division.

--- Tests/NBKCoreKitTests/Private/NBK+Division.swift
+++ Tests/NBKCoreKitTests/Private/NBK+Division.swift
@@ -12,6 +12,10 @@
 import NBKCoreKit
 import XCTest
 
+private typealias W = [UInt]
+private typealias X = [UInt64]
+private typealias Y = [UInt32]
+
 //*============================================================================*
 // MARK: * NBK x Division x Int or UInt
 //*============================================================================*
@@ -52,6 +56,37 @@ final class NBKTestsOnDivisionAsIntOrUInt: XCTestCase {
 }
 
 //*============================================================================*
+// MARK: * NBK x Division x Digit x Unsigned
+//*============================================================================*
+
+final class NBKTestsOnDivisionByDigitAsUnsigned: XCTestCase {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testDividingByDigit() {
+        NBKAssertDivisionByDigit([ ] as W, UInt(1), [ ] as W, UInt( ))
+        NBKAssertDivisionByDigit([ ] as W, UInt(2), [ ] as W, UInt( ))
+        NBKAssertDivisionByDigit([7] as W, UInt(1), [7] as W, UInt( ))
+        NBKAssertDivisionByDigit([7] as W, UInt(2), [3] as W, UInt(1))
+    }
+    
+    func testDividingByDigitReportingOverflow() {
+        NBKAssertDivisionByDigit([ ] as W, UInt( ), [ ] as W, UInt( ), true)
+        NBKAssertDivisionByDigit([1] as W, UInt( ), [1] as W, UInt(1), true)
+        NBKAssertDivisionByDigit([2] as W, UInt( ), [2] as W, UInt(2), true)
+    }
+    
+    func testDividingByDigitWithLargeDividend() {
+        NBKAssertDivisionByDigit([~2,  ~4,  ~6,  9] as W, UInt(2), [~1, ~2, ~3, 4] as W, UInt(1))
+        NBKAssertDivisionByDigit([~3,  ~6,  ~9, 14] as W, UInt(3), [~1, ~2, ~3, 4] as W, UInt(2))
+        NBKAssertDivisionByDigit([~4,  ~8, ~12, 19] as W, UInt(4), [~1, ~2, ~3, 4] as W, UInt(3))
+        NBKAssertDivisionByDigit([~5, ~10, ~15, 24] as W, UInt(5), [~1, ~2, ~3, 4] as W, UInt(4))
+    }
+}
+
+//*============================================================================*
 // MARK: * NBK x Division x Assertions
 //*============================================================================*
 
@@ -65,6 +100,23 @@ file: StaticString = #file, line: UInt = #line) {
     if  let value = Int(exactly: value), let quotient = Int(exactly: quotient), let remainder = Int(exactly: remainder) {
         XCTAssertEqual(NBK .quotientDividingByBitWidthAssumingIsAtLeastZero(value), quotient,  file: file, line: line)
         XCTAssertEqual(NBK.remainderDividingByBitWidthAssumingIsAtLeastZero(value), remainder, file: file, line: line)
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: + Digit
+//=----------------------------------------------------------------------------=
+
+private func NBKAssertDivisionByDigit<T: NBKFixedWidthInteger & NBKUnsignedInteger>(
+_ lhs: [T], _ rhs: T, _ quotient: [T], _ remainder: T, _ overflow: Bool = false,
+file: StaticString = #file, line: UInt = #line) {
+    //=------------------------------------------=
+    formQuotientWithRemainderReportingOverflowAsLenientUnsignedInteger: do {
+        var lhs = lhs
+        let pvo = NBK.formQuotientWithRemainderReportingOverflowAsLenientUnsignedInteger(&lhs, dividingBy: rhs)
+        XCTAssertEqual(lhs,              quotient,  file: file, line: line)
+        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
+        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
     }
 }
 
