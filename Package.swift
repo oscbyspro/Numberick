be5be3ad13e126de39ef91460d8c3e4c93fafd95
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-09-14 16:54:03 +0200

    No flexible in-memory 2's complement (#44).

--- Package.swift
+++ Package.swift
@@ -54,12 +54,6 @@ var package = Package(
         .library(
         name: "NBKFlexibleWidthKit",
         targets: ["NBKFlexibleWidthKit"]),
-        //=--------------------------------------=
-        // NBK x Two's Complement Kit
-        //=--------------------------------------=
-        .library(
-        name: "NBKTwosComplementKit",
-        targets: ["NBKTwosComplementKit"]),
     ],
     targets: [
         //=--------------------------------------=
@@ -110,20 +104,6 @@ var package = Package(
         .testTarget(
         name: "NBKFlexibleWidthKitTests",
         dependencies: ["NBKFlexibleWidthKit"]),
-        //=--------------------------------------=
-        // NBK x Two's Complement Kit
-        //=--------------------------------------=
-        .target(
-        name: "NBKTwosComplementKit",
-        dependencies: ["NBKCoreKit"]),
-        
-        .testTarget(
-        name: "NBKTwosComplementKitBenchmarks",
-        dependencies: ["NBKTwosComplementKit"]),
-        
-        .testTarget(
-        name: "NBKTwosComplementKitTests",
-        dependencies: ["NBKTwosComplementKit"]),
     ]
 )
 
