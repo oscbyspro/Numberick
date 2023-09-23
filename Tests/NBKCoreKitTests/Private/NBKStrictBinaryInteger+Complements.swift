11b9ef2e8b1be8e313174395d24e7aad673673d3
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-09-23 12:15:50 +0200

    [NBKCoreKit] Complements.

---
+++ Tests/NBKCoreKitTests/Private/NBKStrictBinaryInteger+Complements.swift
@@ -0,0 +1,70 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Numberick open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+#if DEBUG
+
+import NBKCoreKit
+import XCTest
+
+//*============================================================================*
+// MARK: * NBK x Strict Binary Integer x Complements x Sub Sequence
+//*============================================================================*
+
+final class NBKStrictBinaryIntegerTestsOnComplementsAsSubSequence: XCTestCase {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testOnesComplement() {
+        NBKAssertOnesComplement([ 0,  0,  0,  0], [~0, ~0, ~0, ~0])
+        NBKAssertOnesComplement([~0, ~0, ~0, ~0], [ 0,  0,  0,  0])
+        NBKAssertOnesComplement([ 1,  2,  3,  4], [~1, ~2, ~3, ~4])
+        NBKAssertOnesComplement([~1, ~2, ~3, ~4], [ 1,  2,  3,  4])
+    }
+    
+    func testTwosComplement() {
+        NBKAssertTwosComplement([ 0,  0,  0,  0], [ 0,  0,  0,  0])
+        NBKAssertTwosComplement([~0, ~0, ~0, ~0], [ 1,  0,  0,  0])
+        NBKAssertTwosComplement([ 1,  2,  3,  4], [~0, ~2, ~3, ~4])
+        NBKAssertTwosComplement([~1, ~2, ~3, ~4], [ 2,  2,  3,  4])
+    }
+}
+
+//*============================================================================*
+// MARK: * NBK x Strict Binary Integer x Complements x Assertions
+//*============================================================================*
+
+private func NBKAssertOnesComplement(
+_ operand: [UInt], _ result: [UInt],
+file: StaticString = #file, line: UInt  = #line) {
+    //=------------------------------------------=
+    typealias SBI = NBK.StrictBinaryInteger<[UInt]>.SubSequence
+    //=------------------------------------------=
+    brr: do {
+        var operand = operand
+        SBI.formOnesComplement(&operand)
+        XCTAssertEqual(operand, result, file: file, line: line)
+    }
+}
+
+private func NBKAssertTwosComplement(
+_ operand: [UInt], _ result: [UInt],
+file: StaticString = #file, line: UInt  = #line) {
+    //=------------------------------------------=
+    typealias SBI = NBK.StrictBinaryInteger<[UInt]>.SubSequence
+    //=------------------------------------------=
+    brr: do {
+        var operand = operand
+        SBI.formTwosComplement(&operand)
+        XCTAssertEqual(operand, result, file: file, line: line)
+    }
+}
+
+#endif
