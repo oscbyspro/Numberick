294446daeb20facedd6e28206d9d31dfab525753
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-10-18 18:43:10 +0200

    [Slices no bounds] Division (#102).

--- Sources/NBKCoreKit/Private/NBKStrictUnsignedInteger+Division.swift
+++ Sources/NBKCoreKit/Private/NBKStrictUnsignedInteger+Division.swift
@@ -19,44 +19,21 @@ extension NBK.StrictUnsignedInteger.SubSequence {
     //=------------------------------------------------------------------------=
     // MARK: Transformations x Overflow
     //=------------------------------------------------------------------------=
-    
+
     /// Returns the `remainder` of dividing the `base` by the `divisor`,
     /// along with an `overflow` indicator.
     ///
-    /// - Note: In the case of `overflow`, the result is `base.first`.
+    /// - Note: In the case of `overflow`, the result is the first element in the given `range`
+    /// or zero if it the given `range` is empty.
     ///
     @inlinable public static func remainderReportingOverflow(
     _   base: Base, dividingBy divisor: Base.Element) -> PVO<Base.Element> {
-        let range = Range(uncheckedBounds:(base.startIndex, base.endIndex))
-        return self.remainderReportingOverflow(base, dividingBy: divisor, in: range)
-    }
-    
-    /// Returns the `remainder` of dividing the `base` by the `divisor` in the given `range`,
-    /// along with an `overflow` indicator.
-    ///
-    /// - Note: In the case of `overflow`, the result is the first element in the given `range`
-    /// or zero if it the given `range` is empty.
-    ///
-    @inlinable public static func remainderReportingOverflow(
-    _   base: Base, dividingBy divisor: Base.Element, in range: some RangeExpression<Base.Index>) -> PVO<Base.Element> {
-        let range = range.relative(to: base) as Range<Base.Index>
-        return self.remainderReportingOverflow(base, dividingBy: divisor, in: range)
-    }
-    
-    /// Returns the `remainder` of dividing the `base` by the `divisor` in the given `range`,
-    /// along with an `overflow` indicator.
-    ///
-    /// - Note: In the case of `overflow`, the result is the first element in the given `range`
-    /// or zero if it the given `range` is empty.
-    ///
-    @inlinable public static func remainderReportingOverflow(
-    _   base: Base, dividingBy divisor: Base.Element, in range: Range<Base.Index>) -> PVO<Base.Element> {
         //=--------------------------------------=
         guard let divisor = NBK.NonZero(exactly: divisor) else {
-            return PVO(range.isEmpty ? 0 as Base.Element : base[range.lowerBound], true)
+            return PVO(base.first ?? 000 as Base.Element, true)
         }
         //=--------------------------------------=
-        return PVO(self.remainder(base, dividingBy: divisor, in: range), false)
+        return PVO(self.remainder(base, dividingBy: divisor), false)
     }
     
     //=------------------------------------------------------------------------=
@@ -66,27 +43,15 @@ extension NBK.StrictUnsignedInteger.SubSequence {
     /// Returns the `remainder` of dividing the `base` by the `divisor`.
     @inlinable public static func remainder(
     _   base: Base, dividingBy divisor: NBK.NonZero<Base.Element>) -> Base.Element {
-        let range = Range(uncheckedBounds:(base.startIndex, base.endIndex))
-        return self.remainder(base, dividingBy: divisor, in: range)
-    }
-    
-    /// Returns the `remainder` of dividing the `base` by the `divisor` in the given `range`.
-    @inlinable public static func remainder(
-    _   base: Base, dividingBy divisor: NBK.NonZero<Base.Element>, in range: some RangeExpression<Base.Index>) -> Base.Element {
-        let range = range.relative(to: base) as Range<Base.Index>
-        return self.remainder(base, dividingBy: divisor, in: range)
-    }
-    
-    /// Returns the `remainder` of dividing the `base` by the `divisor` in the given `range`.
-    @inlinable public static func remainder(
-    _   base: Base, dividingBy divisor: NBK.NonZero<Base.Element>, in range: Range<Base.Index>) -> Base.Element {
+        //=--------------------------------------=
         var remainder = 0 as Base.Element
-        
-        var index = range.upperBound; while index > range.lowerBound {
+        var index = base.endIndex as Base.Index
+        //=--------------------------------------=
+        backwards: while index > base.startIndex {
             base.formIndex(before: &index)
             remainder = divisor.value.dividingFullWidth(HL(high: remainder, low: base[index])).remainder
         }
-        
+        //=--------------------------------------=
         return remainder  as Base.Element
     }
 }
@@ -101,73 +66,38 @@ extension NBK.StrictUnsignedInteger.SubSequence where Base: MutableCollection {
     // MARK: Transformations x Overflow
     //=------------------------------------------------------------------------=
     
-    /// Forms the `quotient` of dividing the `base` by the `divisor`,
-    /// and returns the `remainder` along with an `overflow` indicator.
+    /// Forms the `quotient` of dividing the `base` by the `divisor`, and returns
+    /// the `remainder` along with an `overflow` indicator.
     ///
-    /// - Note: In the case of `overflow`, the result is `base` and `base.first`.
+    /// - Note: In the case of `overflow`, the result is `base` and the first element in the given `range`
+    /// or zero if it the given `range` is empty.
     ///
     @inlinable public static func formQuotientWithRemainderReportingOverflow(
     _   base: inout Base, dividingBy divisor: Base.Element) -> PVO<Base.Element> {
-        let range = Range(uncheckedBounds:(base.startIndex, base.endIndex))
-        return self.formQuotientWithRemainderReportingOverflow(&base, dividingBy: divisor, in: range)
-    }
-    
-    /// Forms the `quotient` of dividing the `base` by the `divisor` in the given `range`,
-    /// and returns the `remainder` along with an `overflow` indicator.
-    ///
-    /// - Note: In the case of `overflow`, the result is `base` and the first element in the given `range`
-    /// or zero if it the given `range` is empty.
-    ///
-    @inlinable public static func formQuotientWithRemainderReportingOverflow(
-    _   base: inout Base, dividingBy divisor: Base.Element, in range: some RangeExpression<Base.Index>) -> PVO<Base.Element> {
-        let range = range.relative(to: base) as Range<Base.Index>
-        return self.formQuotientWithRemainderReportingOverflow(&base, dividingBy: divisor, in: range)
-    }
-    
-    /// Forms the `quotient` of dividing the `base` by the `divisor` in the given `range`,
-    /// and returns the `remainder` along with an `overflow` indicator.
-    ///
-    /// - Note: In the case of `overflow`, the result is `base` and the first element in the given `range`
-    /// or zero if it the given `range` is empty.
-    ///
-    @inlinable public static func formQuotientWithRemainderReportingOverflow(
-    _   base: inout Base, dividingBy divisor: Base.Element, in range: Range<Base.Index>) -> PVO<Base.Element> {
         //=--------------------------------------=
         guard let divisor = NBK.NonZero(exactly: divisor) else {
-            return PVO(range.isEmpty ? 0 as Base.Element : base[range.lowerBound], true)
+            return PVO(base.first ?? 000 as Base.Element, true)
         }
         //=--------------------------------------=
-        return PVO(self.formQuotientWithRemainder(&base, dividingBy: divisor, in: range), false)
+        return PVO(self.formQuotientWithRemainder(&base, dividingBy: divisor), false)
     }
     
     //=------------------------------------------------------------------------=
     // MARK: Transformations x where Divisor is Non Zero
     //=------------------------------------------------------------------------=
     
-    /// Forms the `quotient` of dividing the `base` by the `divisor`.
+    /// Forms the `quotient` of dividing the `base` by the `divisor`, and returns the `remainder`.
     @inlinable public static func formQuotientWithRemainder(
     _   base: inout Base, dividingBy divisor: NBK.NonZero<Base.Element>) -> Base.Element {
-        let range = Range(uncheckedBounds:(base.startIndex, base.endIndex))
-        return self.formQuotientWithRemainder(&base, dividingBy: divisor, in: range)
-    }
-    
-    /// Forms the `quotient` of dividing the `base` by the `divisor` in the given `range`.
-    @inlinable public static func formQuotientWithRemainder(
-    _   base: inout Base, dividingBy divisor: NBK.NonZero<Base.Element>, in range: some RangeExpression<Base.Index>) -> Base.Element {
-        let range = range.relative(to: base) as Range<Base.Index>
-        return self.formQuotientWithRemainder(&base, dividingBy: divisor, in: range)
-    }
-    
-    /// Forms the `quotient` of dividing the `base` by the `divisor` in the given `range`, and returns the `remainder`.
-    @inlinable public static func formQuotientWithRemainder(
-    _   base: inout Base, dividingBy divisor: NBK.NonZero<Base.Element>, in range: Range<Base.Index>) -> Base.Element {
+        //=--------------------------------------=
         var remainder = 0 as Base.Element
-        
-        var index = range.upperBound; while index > range.lowerBound {
+        var index = base.endIndex as Base.Index
+        //=--------------------------------------=
+        backwards: while index > base.startIndex {
             base.formIndex(before: &index)
            (base[index], remainder) = divisor.value.dividingFullWidth(HL(high: remainder, low: base[index]))
         }
-        
+        //=--------------------------------------=
         return remainder  as Base.Element
     }
 }
--- Tests/NBKCoreKitTests/Private/NBKStrictUnsignedInteger+Division.swift
+++ Tests/NBKCoreKitTests/Private/NBKStrictUnsignedInteger+Division.swift
@@ -27,50 +27,31 @@ final class NBKStrictUnsignedIntegerTestsOnDivisionAsSubSequence: XCTestCase {
     //=------------------------------------------------------------------------=
     
     func testDividingSmallBySmall() {
-        NBKAssertDivisionByDigitInRange([ ] as W, UInt(1), 0 ..< 0, [ ] as W, UInt( ))
-        NBKAssertDivisionByDigitInRange([ ] as W, UInt(2), 0 ..< 0, [ ] as W, UInt( ))
-        NBKAssertDivisionByDigitInRange([0] as W, UInt(1), 0 ..< 1, [0] as W, UInt( ))
-        NBKAssertDivisionByDigitInRange([0] as W, UInt(2), 0 ..< 1, [0] as W, UInt( ))
-        NBKAssertDivisionByDigitInRange([7] as W, UInt(1), 0 ..< 1, [7] as W, UInt( ))
-        NBKAssertDivisionByDigitInRange([7] as W, UInt(2), 0 ..< 1, [3] as W, UInt(1))
+        NBKAssertDivisionByDigit([ ] as W, UInt(1), [ ] as W, UInt( ))
+        NBKAssertDivisionByDigit([ ] as W, UInt(2), [ ] as W, UInt( ))
+        NBKAssertDivisionByDigit([0] as W, UInt(1), [0] as W, UInt( ))
+        NBKAssertDivisionByDigit([0] as W, UInt(2), [0] as W, UInt( ))
+        NBKAssertDivisionByDigit([7] as W, UInt(1), [7] as W, UInt( ))
+        NBKAssertDivisionByDigit([7] as W, UInt(2), [3] as W, UInt(1))
     }
     
     func testDividingSmallBySmallReportingOverflow() {
-        NBKAssertDivisionByDigitInRange([ ] as W, UInt( ), 0 ..< 0, [ ] as W, UInt( ), true)
-        NBKAssertDivisionByDigitInRange([0] as W, UInt( ), 0 ..< 1, [0] as W, UInt( ), true)
-        NBKAssertDivisionByDigitInRange([1] as W, UInt( ), 0 ..< 1, [1] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([2] as W, UInt( ), 0 ..< 1, [2] as W, UInt(2), true)
+        NBKAssertDivisionByDigit([ ] as W, UInt( ), [ ] as W, UInt( ), true)
+        NBKAssertDivisionByDigit([0] as W, UInt( ), [0] as W, UInt( ), true)
+        NBKAssertDivisionByDigit([1] as W, UInt( ), [1] as W, UInt(1), true)
+        NBKAssertDivisionByDigit([2] as W, UInt( ), [2] as W, UInt(2), true)
     }
     
     func testDividingLargeBySmallWithLargeQuotient() {
-        NBKAssertDivisionByDigitInRange([~2,  ~4,  ~6,  9] as W, UInt(2), 0 ..< 0, [~2,  ~4/1,  ~6/1,  9] as W, UInt(0))
-        NBKAssertDivisionByDigitInRange([~3,  ~6,  ~9, 14] as W, UInt(3), 0 ..< 0, [~3,  ~6/1,  ~9/1, 14] as W, UInt(0))
-        NBKAssertDivisionByDigitInRange([~4,  ~8, ~12, 19] as W, UInt(4), 0 ..< 0, [~4,  ~8/1, ~12/1, 19] as W, UInt(0))
-        NBKAssertDivisionByDigitInRange([~5, ~10, ~15, 24] as W, UInt(5), 0 ..< 0, [~5, ~10/1, ~15/1, 24] as W, UInt(0))
-        
-        NBKAssertDivisionByDigitInRange([~2,  ~4,  ~6,  9] as W, UInt(2), 1 ..< 3, [~2,  ~2/1,  ~6/2,  9] as W, UInt(1))
-        NBKAssertDivisionByDigitInRange([~3,  ~6,  ~9, 14] as W, UInt(3), 1 ..< 3, [~3,  ~6/3,  ~9/3, 14] as W, UInt(0))
-        NBKAssertDivisionByDigitInRange([~4,  ~8, ~12, 19] as W, UInt(4), 1 ..< 3, [~4,  ~2/1, ~12/4, 19] as W, UInt(3))
-        NBKAssertDivisionByDigitInRange([~5, ~10, ~15, 24] as W, UInt(5), 1 ..< 3, [~5, ~10/5, ~15/5, 24] as W, UInt(0))
-        
-        NBKAssertDivisionByDigitInRange([~2,  ~4,  ~6,  9] as W, UInt(2), 0 ..< 4, [~1,  ~2/1,  ~3/1,  4] as W, UInt(1))
-        NBKAssertDivisionByDigitInRange([~3,  ~6,  ~9, 14] as W, UInt(3), 0 ..< 4, [~1,  ~2/1,  ~3/1,  4] as W, UInt(2))
-        NBKAssertDivisionByDigitInRange([~4,  ~8, ~12, 19] as W, UInt(4), 0 ..< 4, [~1,  ~2/1,  ~3/1,  4] as W, UInt(3))
-        NBKAssertDivisionByDigitInRange([~5, ~10, ~15, 24] as W, UInt(5), 0 ..< 4, [~1,  ~2/1,  ~3/1,  4] as W, UInt(4))
+        NBKAssertDivisionByDigit([~2,  ~4,  ~6,  9] as W, UInt(2), [~1,  ~2/1,  ~3/1,  4] as W, UInt(1))
+        NBKAssertDivisionByDigit([~3,  ~6,  ~9, 14] as W, UInt(3), [~1,  ~2/1,  ~3/1,  4] as W, UInt(2))
+        NBKAssertDivisionByDigit([~4,  ~8, ~12, 19] as W, UInt(4), [~1,  ~2/1,  ~3/1,  4] as W, UInt(3))
+        NBKAssertDivisionByDigit([~5, ~10, ~15, 24] as W, UInt(5), [~1,  ~2/1,  ~3/1,  4] as W, UInt(4))
     }
     
     func testDividingLargeBySmallReportingOverflow() {
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 4, [1, 2, 3, 4] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 3, [1, 2, 3, 4] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 2, [1, 2, 3, 4] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 1, [1, 2, 3, 4] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 0, [1, 2, 3, 4] as W, UInt( ), true)
-        
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 0 ..< 4, [1, 2, 3, 4] as W, UInt(1), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 1 ..< 4, [1, 2, 3, 4] as W, UInt(2), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 2 ..< 4, [1, 2, 3, 4] as W, UInt(3), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 3 ..< 4, [1, 2, 3, 4] as W, UInt(4), true)
-        NBKAssertDivisionByDigitInRange([1, 2, 3, 4] as W, UInt( ), 4 ..< 4, [1, 2, 3, 4] as W, UInt( ), true)
+        NBKAssertDivisionByDigit([1, 2, 3, 4] as W, UInt( ), [1, 2, 3, 4] as W, UInt(1), true)
+        NBKAssertDivisionByDigit([1, 2, 3, 4] as W, UInt( ), [1, 2, 3, 4] as W, UInt(1), true)
     }
 }
 
@@ -78,125 +59,47 @@ final class NBKStrictUnsignedIntegerTestsOnDivisionAsSubSequence: XCTestCase {
 // MARK: * NBK x Strict Unsigned Integer x Division x Assertions
 //*============================================================================*
 
-private func NBKAssertDivisionByDigitInRange(
-_ lhs: [UInt], _ rhs: UInt, _ range: Range<Int>, _ quotient: [UInt], _ remainder: UInt, _ overflow: Bool = false,
+private func NBKAssertDivisionByDigitWithNonZeroDivisor(
+_ lhs: [UInt], _ rhs: NBK.NonZero<UInt>, _ quotient: [UInt], _ remainder: UInt, _ overflow: Bool = false,
 file: StaticString = #file, line: UInt = #line) {
     //=------------------------------------------=
-    typealias T = NBK.StrictUnsignedInteger<[UInt]>.SubSequence
+    typealias T = NBK.SUISS
+    //=------------------------------------------=
+    brr: do {
+        let rem = T.remainder(lhs, dividingBy: rhs)
+        XCTAssertEqual(rem, remainder, file: file, line: line)
+    }
+    
+    brr: do {
+        var lhs = lhs
+        let rem = T.formQuotientWithRemainder(&lhs, dividingBy: rhs)
+        XCTAssertEqual(lhs, quotient,  file: file, line: line)
+        XCTAssertEqual(rem, remainder, file: file, line: line)
+    }
+}
+
+private func NBKAssertDivisionByDigit(
+_ lhs: [UInt], _ rhs: UInt, _ quotient: [UInt], _ remainder: UInt, _ overflow: Bool = false,
+file: StaticString = #file, line: UInt = #line) {
+    //=------------------------------------------=
+    typealias T = NBK.SUISS
     //=------------------------------------------=
     if  let rhs = NBK.NonZero(exactly: rhs) {
-        NBKAssertDivisionByDigitInRangeWithNonZeroDivisor(lhs, rhs, range, quotient, remainder, overflow, file: file, line: line)
+        NBKAssertDivisionByDigitWithNonZeroDivisor(lhs, rhs, quotient, remainder, overflow, file: file, line: line)
     }
     //=------------------------------------------=
-    brrrrrrrrrrr: do {
-        let pvo = T.remainderReportingOverflow(lhs, dividingBy: rhs, in: range)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    brrrrrrrrrrr: do {
-        var lhs = lhs
-        let pvo = T.formQuotientWithRemainderReportingOverflow(&lhs, dividingBy: rhs, in: range)
-        XCTAssertEqual(lhs,              quotient,  file: file, line: line)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex {
-        let pvo = T.remainderReportingOverflow(lhs, dividingBy: rhs, in: ..<range.upperBound)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex {
-        var lhs = lhs
-        let pvo = T.formQuotientWithRemainderReportingOverflow(&lhs, dividingBy: rhs, in: ..<range.upperBound)
-        XCTAssertEqual(lhs,              quotient,  file: file, line: line)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    if  range.upperBound == lhs.endIndex {
-        let pvo = T.remainderReportingOverflow(lhs, dividingBy: rhs, in: range.lowerBound...)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    if  range.upperBound == lhs.endIndex {
-        var lhs = lhs
-        let pvo = T.formQuotientWithRemainderReportingOverflow(&lhs, dividingBy: rhs, in: range.lowerBound...)
-        XCTAssertEqual(lhs,              quotient,  file: file, line: line)
-        XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
-        XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex, range.upperBound == lhs.endIndex {
+    brr: do {
         let pvo = T.remainderReportingOverflow(lhs, dividingBy: rhs)
         XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
         XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
     }
     
-    if  range.lowerBound == lhs.startIndex, range.upperBound == lhs.endIndex {
+    brr: do {
         var lhs = lhs
         let pvo = T.formQuotientWithRemainderReportingOverflow(&lhs, dividingBy: rhs)
         XCTAssertEqual(lhs,              quotient,  file: file, line: line)
         XCTAssertEqual(pvo.partialValue, remainder, file: file, line: line)
         XCTAssertEqual(pvo.overflow,     overflow,  file: file, line: line)
-    }
-}
-
-private func NBKAssertDivisionByDigitInRangeWithNonZeroDivisor(
-_ lhs: [UInt], _ rhs: NBK.NonZero<UInt>, _ range: Range<Int>, _ quotient: [UInt], _ remainder: UInt, _ overflow: Bool = false,
-file: StaticString = #file, line: UInt = #line) {
-    //=------------------------------------------=
-    typealias T = NBK.StrictUnsignedInteger<[UInt]>.SubSequence
-    //=------------------------------------------=
-    brrrrrrrrrrr: do {
-        let rem = T.remainder(lhs, dividingBy: rhs, in: range)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    brrrrrrrrrrr: do {
-        var lhs = lhs
-        let rem = T.formQuotientWithRemainder(&lhs, dividingBy: rhs, in: range)
-        XCTAssertEqual(lhs, quotient,  file: file, line: line)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex {
-        let rem = T.remainder(lhs, dividingBy: rhs, in: ..<range.upperBound)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex {
-        var lhs = lhs
-        let rem = T.formQuotientWithRemainder(&lhs, dividingBy: rhs, in: ..<range.upperBound)
-        XCTAssertEqual(lhs, quotient,  file: file, line: line)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.upperBound == lhs.endIndex {
-        let rem = T.remainder(lhs, dividingBy: rhs, in: range.lowerBound...)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.upperBound == lhs.endIndex {
-        var lhs = lhs
-        let rem = T.formQuotientWithRemainder(&lhs, dividingBy: rhs, in: range.lowerBound...)
-        XCTAssertEqual(lhs, quotient,  file: file, line: line)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex, range.upperBound == lhs.endIndex {
-        let rem = T.remainder(lhs, dividingBy: rhs)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
-    }
-    
-    if  range.lowerBound == lhs.startIndex, range.upperBound == lhs.endIndex {
-        var lhs = lhs
-        let rem = T.formQuotientWithRemainder(&lhs, dividingBy: rhs)
-        XCTAssertEqual(lhs, quotient,  file: file, line: line)
-        XCTAssertEqual(rem, remainder, file: file, line: line)
     }
 }
 
